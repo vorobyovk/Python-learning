6d668cf77aa3801a4cdc2e971a46f11c13806e0f
Vorobyov Kirill <vorobyovk@gmail.com>
2019-02-14 19:05:52 +0200

    +

--- Section 2, lesson 22.py
+++ Section 2, lesson 22.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 print(sys.argv)
 print("-----------------------------------")
@@ -6,3 +7,15 @@ i = 0
 for inputing in sys.argv:
     i =+ i
     print(str(inputing)[i]+"\n")
+print("----------------------------------")
+
+x = len(sys.argv)
+for j in range(1,x,1):
+    if sys.argv[j] == "h":
+        print("Help option")
+    elif sys.argv[j] == "c":
+        print("Hello world")
+    else:
+        print("I don`t know to print!")
+
+
